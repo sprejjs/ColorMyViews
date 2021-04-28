36dc9d557ff0c890437c9fae3178db6eeafbf401
Vlad Spreys <vlad@spreys.com>
2021-04-28 13:18:31 +1000

    feat: Added on click listeners

--- app/src/main/java/com/spreys/colormyviews/MainActivity.kt
+++ app/src/main/java/com/spreys/colormyviews/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.spreys.colormyviews
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.spreys.colormyviews.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.apply {
+            arrayOf<View>(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText).forEach {
+                it.setOnClickListener { handleClick(it) }
+            }
+        }
+    }
+
+    private fun handleClick(view: View) {
+        binding.apply {
+            when (view) {
+                boxOneText -> view.setBackgroundColor(Color.DKGRAY)
+                boxTwoText -> view.setBackgroundColor(Color.GRAY)
+                boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
+                boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
+                boxFiveText -> view.setBackgroundResource(android.R.color.holo_green_light)
+                else -> view.setBackgroundColor(Color.LTGRAY)
+            }
+        }
     }
 }
