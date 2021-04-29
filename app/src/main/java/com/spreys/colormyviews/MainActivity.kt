1df087576e385b2417b247a0ef198a6af45ca47b
Vlad Spreys <vlad@spreys.com>
2021-04-29 11:03:46 +1000

    feat: Coloring buttons

--- app/src/main/java/com/spreys/colormyviews/MainActivity.kt
+++ app/src/main/java/com/spreys/colormyviews/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.spreys.colormyviews
 
+import android.content.res.ColorStateList
 import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
 import com.spreys.colormyviews.databinding.ActivityMainBinding
 
@@ -15,8 +19,11 @@ class MainActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.apply {
-            arrayOf<View>(boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText).forEach {
-                it.setOnClickListener { handleClick(it) }
+            arrayOf<View>(
+                boxOneText, boxTwoText, boxThreeText, boxFourText, boxFiveText, rootElement,
+                greenButton, redButton, yellowButton
+            ).forEach { view ->
+                view.setOnClickListener { handleClick(it) }
             }
         }
     }
@@ -29,8 +36,20 @@ class MainActivity : AppCompatActivity() {
                 boxThreeText -> view.setBackgroundResource(android.R.color.holo_green_light)
                 boxFourText -> view.setBackgroundResource(android.R.color.holo_green_dark)
                 boxFiveText -> view.setBackgroundResource(android.R.color.holo_green_light)
+                greenButton -> setBackgroundColor(view, R.color.my_green)
+                yellowButton -> setBackgroundColor(view, R.color.my_yellow)
+                redButton -> setBackgroundColor(view, R.color.my_red)
                 else -> view.setBackgroundColor(Color.LTGRAY)
             }
+        }
+    }
+
+    private fun setBackgroundColor(button: View, colorResourceId: Int) {
+        val color = ContextCompat.getColor(this@MainActivity, colorResourceId)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            button.backgroundTintList = ColorStateList.valueOf(color)
+        } else {
+            ViewCompat.setBackgroundTintList(button, ColorStateList.valueOf(color));
         }
     }
 }
